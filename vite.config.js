01603b85d7b11f6c98aeb4febc3125d51533bc7b
Justin Newbold <justinnewbold@me.com>
2025-12-21 20:52:55 -0700

    feat: Configure Vite PWA plugin for offline support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon.svg', 'icons/*.png'],
+      manifest: {
+        name: 'TimeFlow - Focus & Productivity',
+        short_name: 'TimeFlow',
+        description: 'Master your day with focused time blocking and Pomodoro timer',
+        theme_color: '#FF6B6B',
+        background_color: '#0f0f1a',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          { src: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+          { src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: { cacheName: 'google-fonts-cache', expiration: { maxEntries: 10, maxAgeSeconds: 31536000 } }
+          }
+        ]
+      }
+    })
+  ],
+  build: { outDir: 'dist', sourcemap: false }
 })
